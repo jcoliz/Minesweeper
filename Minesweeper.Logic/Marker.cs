66e1c2ae9763957eb2dcbfa5468b212e04558382
James Coliz <jcoliz@mail.com>
2018-09-29 16:33:12 -0700

    Added new marker class

---
+++ Minesweeper.Logic/Marker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minesweeper.Logic
+{
+    public class Marker
+    {
+        /// <summary>
+        /// Whether there is a bomb at this marker
+        /// </summary>
+        public bool isBomb { get; set; }
+
+        /// <summary>
+        /// Whether the marker is showing its contents to the user (true) or hidden (false)
+        /// </summary>
+        public bool isShowing { get; set; }
+
+        /// <summary>
+        /// How many neighbors have bombs
+        /// </summary>
+        public int NumNearbyBombs { get; set; }
+    }
+}
