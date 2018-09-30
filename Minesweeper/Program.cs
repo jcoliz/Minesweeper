90bf5be2bd74f821b3f635f8f7ce271c9ddf122e
James Coliz <jcoliz@mail.com>
2018-09-29 22:09:56 -0700

    Show field at the end and await an extra enter

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -6,41 +6,21 @@ namespace Minesweeper
 {
     class Program
     {
+        static MarkerGrid Playfield;
+        const int Size = 9;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Minesweeper!");
 
-            var size = 9;
-            var playfield = new MarkerGrid(size);
-
-            var headers = new string[3] { "   ", "   ", "   " };
-            for(int i = 1; i <= size; ++i)
-            {
-                headers[0] += (i / 10).ToString();
-                headers[1] += (i % 10).ToString();
-                headers[2] += '-';
-            }
-            var footers = new string[3] { headers[2], headers[1], headers[0] };
+            Playfield = new MarkerGrid(Size);
 
             bool done = false;
             while(!done)
             {
                 try
                 {
-                    var lines = playfield.Render();
-
-                    Console.WriteLine();
-                    foreach (var header in headers)
-                        Console.WriteLine(header);
-                    int lineno = 1;
-                    foreach (var line in lines)
-                    {
-                        Console.WriteLine($"{lineno,02}|{line}|{lineno,02}");
-                        ++lineno;
-                    }
-                    foreach (var footer in footers)
-                        Console.WriteLine(footer);
-                    Console.WriteLine();
+                    Render();
 
                     Console.Write("Enter col,row to play or q to quit> ");
 
@@ -54,7 +34,7 @@ namespace Minesweeper
                     int col = int.Parse(values[0]) - 1;
                     int row = int.Parse(values[1]) - 1;
 
-                    var result = playfield.PlayAt(col, row);
+                    var result = Playfield.PlayAt(col, row);
 
                     Console.WriteLine(result.ToString());
 
@@ -66,6 +46,39 @@ namespace Minesweeper
                     Console.WriteLine(ex.Message);
                 }
             }
+
+            Render();
+
+            Console.Write("Press enter:");
+            Console.ReadLine();
+        }
+
+        static void Render()
+        {
+            var headers = new string[3] { "   ", "   ", "   " };
+            for (int i = 1; i <= Size; ++i)
+            {
+                headers[0] += (i / 10).ToString();
+                headers[1] += (i % 10).ToString();
+                headers[2] += '-';
+            }
+            var footers = new string[3] { headers[2], headers[1], headers[0] };
+
+            var lines = Playfield.Render();
+
+            Console.WriteLine();
+            foreach (var header in headers)
+                Console.WriteLine(header);
+            int lineno = 1;
+            foreach (var line in lines)
+            {
+                Console.WriteLine($"{lineno,02}|{line}|{lineno,02}");
+                ++lineno;
+            }
+            foreach (var footer in footers)
+                Console.WriteLine(footer);
+            Console.WriteLine();
+
         }
     }
 }
