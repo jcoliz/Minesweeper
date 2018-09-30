f225a43216737da4dad089f0183246c92a778865
James Coliz <jcoliz@mail.com>
2018-09-30 14:32:09 -0700

    Refactor board into own

---
+++ Minesweeper.Logic/Board.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Minesweeper.Logic
+{
+    /// <summary>
+    /// Contains a grid of markers for a console board game
+    /// </summary>
+    /// <remarks>
+    /// This could easily be made generic Board(T) where T: new
+    /// </remarks>
+    public class Board
+    {
+        public Size Dimensions { get; }
+        public List<List<Marker>> Markers { get; }
+
+        public Board(Size desireddimensions)
+        {
+            Dimensions = desireddimensions;
+
+            Markers = new List<List<Marker>>();
+
+            for (int row = 0; row < Dimensions.Height; ++row)
+            {
+                var newrow = new List<Marker>();
+                for (int col = 0; col < Dimensions.Width; ++col)
+                {
+                    newrow.Add(new Marker());
+                }
+                Markers.Add(newrow);
+            }
+        }
+
+        /// <summary>
+        /// Render grid in a display-friendly way
+        /// </summary>
+        /// <returns></returns>
+        public List<string> Render()
+        {
+            List<string> result = new List<string>();
+
+            foreach (var row in Markers)
+            {
+                string line = string.Empty;
+
+                foreach (var marker in row)
+                    line = line + marker;
+
+                result.Add(line);
+            }
+
+            return result;
+        }
+    }
+}
--- Minesweeper.Logic/MarkerGrid.cs
+++ Minesweeper.Logic/MarkerGrid.cs
@@ -5,50 +5,9 @@ using System.Text;
 
 namespace Minesweeper.Logic
 {
-    public class Board
-    {
-        public Size Dimensions { get; }
-        public List<List<Marker>> Markers { get; }
-
-        public Board(Size desireddimensions)
-        {
-            Dimensions = desireddimensions;
-
-            Markers = new List<List<Marker>>();
-
-            for (int row = 0; row < Dimensions.Height; ++row)
-            {
-                var newrow = new List<Marker>();
-                for (int col = 0; col < Dimensions.Width; ++col)
-                {
-                    newrow.Add(new Marker());
-                }
-                Markers.Add(newrow);
-            }
-        }
-
-        /// <summary>
-        /// Render grid in a display-friendly way
-        /// </summary>
-        /// <returns></returns>
-        public List<string> Render()
-        {
-            List<string> result = new List<string>();
-
-            foreach (var row in Markers)
-            {
-                string line = string.Empty;
-
-                foreach (var marker in row)
-                    line = line + marker;
-
-                result.Add(line);
-            }
-
-            return result;
-        }
-    }
-
+    /// <summary>
+    /// Contains the game logic for a game of Minesweeper!
+    /// </summary>
     public class Game
     {
         public enum PlayResult { Invalid = 0, Continue, GameOver, Victory };
