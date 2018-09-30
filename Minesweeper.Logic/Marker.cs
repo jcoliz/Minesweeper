34ab74ba6b9e06a546becffac17009c591845b2c
James Coliz <jcoliz@mail.com>
2018-09-30 15:04:58 -0700

    Remove and sort usings

--- Minesweeper.Logic/Marker.cs
+++ Minesweeper.Logic/Marker.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Minesweeper.Logic
+﻿namespace Minesweeper.Logic
 {
     /// <summary>
     /// A single position on a minesweeper game board
